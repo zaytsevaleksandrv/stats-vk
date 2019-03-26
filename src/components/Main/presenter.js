d8e02e97ed807cb18d229d383eb9fa44b4e00fab
azaytsev <zaytsev.aleksandr.v@gmail.com>
2019-03-26 13:04:41 +0400

    edit Main

--- src/components/Main/presenter.js
+++ src/components/Main/presenter.js
@@ -3,11 +3,7 @@ import React from 'react';
 function Main({ tracks = [] }) {
     return (
         <div>
-            {
-                tracks.map((track, key) => {
-                    return <div className="track" key={key}>{track.title}</div>;
-                })
-            }
+            gggg
         </div>
     );
 }
--- src/index.js
+++ src/index.js
@@ -4,22 +4,11 @@ import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 import { syncHistoryWithStore } from 'react-router-redux';
 import { Provider } from 'react-redux';
 import configureStore from './stores/configureStore';
-import * as actions from './actions';
 import App from './components/App';
 import Main from './components/Main';
 import Test from './components/Test';
 
-const tracks = [
-    {
-        title: 'Some track'
-    },
-    {
-        title: 'Some other track'
-    }
-];
-
 const store = configureStore();
-store.dispatch(actions.setTracks(tracks));
 
 const history = syncHistoryWithStore(browserHistory, store);
 

1a2ce4c36cd2c1741c77aba5501f87db10789f72
azaytsev <zaytsev.aleksandr.v@gmail.com>
2019-03-29 01:01:48 +0400

    test vkui

--- src/components/Test/index.js
+++ src/components/Test/index.js
@@ -1,21 +1,69 @@
-import React from 'react';
-import { View, Panel, PanelHeader, Group, List, Cell } from '@vkontakte/vkui';
-import '@vkontakte/vkui/dist/vkui.css';
+import React, { Component } from "react";
+import {
+  Root,
+  CellButton,
+  PanelHeaderEdit,
+  Icon24Add,
+  PanelHeaderClose,
+  PanelHeaderSubmit,
+  PanelSpinner,
+  Avatar,
+  View,
+  Panel,
+  PanelHeader,
+  Group,
+  List,
+  Cell
+} from "@vkontakte/vkui";
+import PanelHeaderBack from "@vkontakte/vkui/dist/components/PanelHeaderBack/PanelHeaderBack";
+import "@vkontakte/vkui/dist/vkui.css";
 
-function Test() {
+export default class Test extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      activeView: "view1"
+    };
+  }
+
+  render() {
     return (
-        <View activePanel="main">
-            <Panel id="main">
-                <PanelHeader>VKUI</PanelHeader>
-                <Group title="Items">
-                    <List>
-                        <Cell>Hello</Cell>
-                        <Cell>World</Cell>
-                    </List>
-                </Group>
-            </Panel>
+      <Root activeView={this.state.activeView}>
+        <View activePanel="panel1.1" id="view1">
+          <Panel id="panel1.1">
+            <PanelHeader>Stats-VK</PanelHeader>
+            <Group>
+              <CellButton
+                onClick={() => this.setState({ activeView: "view2" })}
+              >
+                Перейти к списку
+              </CellButton>
+            </Group>
+          </Panel>
         </View>
+        <View header activePanel="panel2.1" id="view2">
+          <Panel id="panel2.1">
+            <PanelHeader
+              left={
+                <PanelHeaderBack
+                  onClick={() => this.setState({ activeView: "view1" })}
+                />
+              }
+            >
+              View 2
+            </PanelHeader>
+            <Group>
+              <Cell before={<Avatar />} description="Веб-сайт">
+                Команда ВКонтакте
+              </Cell>
+              <Cell before={<Avatar />} description="Веб-сайт">
+                Команда ВКонтакте
+              </Cell>
+            </Group>
+          </Panel>
+        </View>
+      </Root>
     );
+  }
 }
-
-export default Test;

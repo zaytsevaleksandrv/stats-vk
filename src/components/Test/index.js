c03bbcaecc4eb221121a9635283ab8025fd1cdd1
azaytsev <zaytsev.aleksandr.v@gmail.com>
2019-03-29 00:07:28 +0400

    add vkui

--- src/components/Test/index.js
+++ src/components/Test/index.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import { View, Panel, PanelHeader, Group, List, Cell } from '@vkontakte/vkui';
+import '@vkontakte/vkui/dist/vkui.css';
 
 function Test() {
-    return <div>ewdwedewd</div>;
+    return (
+        <View activePanel="main">
+            <Panel id="main">
+                <PanelHeader>VKUI</PanelHeader>
+                <Group title="Items">
+                    <List>
+                        <Cell>Hello</Cell>
+                        <Cell>World</Cell>
+                    </List>
+                </Group>
+            </Panel>
+        </View>
+    );
 }
 
 export default Test;
